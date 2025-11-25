ab8c40ce5617e94a0c0dccbe9b3a4e8849469203
Ardi Renaldi <28975917+renaldimedia@users.noreply.github.com>
2025-11-25 16:37:24 +0700

    Update settings.ts

--- src/pages/api/settings.ts
+++ src/pages/api/settings.ts
@@ -3,16 +3,28 @@ import type { APIRoute } from "astro";
 export const prerender = false; // penting biar tidak di-prerender
 
 export const GET: APIRoute = async ({ url, locals }) => {
-  const env = locals.runtime.env; // akses ke D1 di Cloudflare
+    try {
+        const env = locals.runtime.env; // akses ke D1 di Cloudflare
 
-  const name = url.searchParams.get('name');
+        const name = url.searchParams.get('name');
 
-  const data = await env.DB.prepare(
-    "SELECT setting_value from settings WHERE setting_key = ?"
-  ).bind(name).run();
+        const data = await env.DB.prepare(
+            "SELECT setting_value from settings WHERE setting_key = ?"
+        ).bind(name).run();
 
-  return new Response(JSON.stringify({ success: true, data }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+        return new Response(JSON.stringify({ success: true, data }), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+        });
+    } catch (error: any) {
+        return new Response(JSON.stringify({
+            success: false,
+            message: error.message,
+            stack: error.stack,
+        }), {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+        });
+    }
+
 };
