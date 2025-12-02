bff5044280f3a144c6c61959c75fa01c02f3b64b
Ardi Renaldi <28975917+renaldimedia@users.noreply.github.com>
2025-12-02 17:54:08 +0700

    update seo

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "AppAppID";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Buat Web, Apps, Wordpress, Woocommerce, Toko Online, Landing Page dengan mudah dan cepat bersama AppAppID.";
