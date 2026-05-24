af898c131d24771b64da013a91c925e635635002
Ardi <renaldimedia@gmail.com>
2026-05-24 08:03:46 +0700

    update

--- src/components/OrderButton.jsx
+++ src/components/OrderButton.jsx
@@ -49,7 +49,7 @@ export default function OrderButton({ label = "Pesan Sekarang", message = "Saya 
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                className="w-4 h-4 ml-auto"
+                className="w-4 h-4 ml-2"
                 viewBox="0 0 24 24"
             >
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
